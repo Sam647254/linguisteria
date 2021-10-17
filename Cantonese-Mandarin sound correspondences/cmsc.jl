9c6b0b23de025a0a35eef5c56454d79a8c2ab82f
Sam Wang <smwn@hey.com>
2021-10-16 19:18:16 -0700

    Added plot

--- Cantonese-Mandarin sound correspondences/cmsc.jl
+++ Cantonese-Mandarin sound correspondences/cmsc.jl
@@ -1,11 +1,14 @@
 using JSON
+using OrderedCollections
 using Unicode
+using DataFrames
+using PlotlyJS
 
 # Steps:
 # 1. Parse the file of Simplified Chinese-Pinyin entries
 ENTRY_REGEX = r"^.*\[\[(.*)\]\]</span>\|\|(.*)$"
 
-sc = readlines("simplified.txt")
+sc = readlines("$(dirname(@__FILE__))/simplified.txt")
 
 schars = filter(
    x -> x !== nothing,
@@ -21,7 +24,7 @@ schars = filter(
 
 # 2. Convert the characters to traditional characters. It is possible that a single Simplified
 # character maps to multiple traditional characters, in which case the list will grow.
-s_to_t = readlines("STCharacters.txt")
+s_to_t = readlines("$(dirname(@__FILE__))/STCharacters.txt")
 st_dict = Dict(
    map(s_to_t) do line
       (simplified, traditional) = split(line, '\t')
@@ -35,7 +38,7 @@ tchars = map(schars) do pair
 end |> Iterators.flatten |> collect
 
 # 3. Read the Jyutping dictionary
-jp = JSON.parsefile("chars_to_jyutping.json")
+jp = JSON.parsefile("$(dirname(@__FILE__))/chars_to_jyutping.json")
 
 # 4. Match the two pronunciations
 triples = map(tchars) do pair
@@ -67,13 +70,57 @@ function groupby(f::Function, items)
    collection = Dict()
    for item in items
       key = f(item)
-      push!(get!(collection, key, []), item)
+      if key === nothing
+         continue
+      else
+         push!(get!(collection, key, []), item)
+      end
    end
    return collection
 end
 
 mandarin_tone_groups = groupby(t -> t[2] |> only, triples_single_tones)
 mandarin_tone_counts =
-   Dict(tone => Dict(tone => length(entries)/length(characters)
+   OrderedDict(tone => Dict(tone => length(entries)/length(characters)
       for (tone, entries) in groupby(t -> t[3], characters))
       for (tone, characters) in mandarin_tone_groups)
+
+tones_matrix = zeros(4, 6)
+
+for (mandarin_tone, cantonese_tones) in mandarin_tone_counts
+   for (cantonese_tone, stat) in cantonese_tones
+      tones_matrix[mandarin_tone, cantonese_tone] = stat
+   end
+end
+
+# Plot the tone correspondences
+MANDARIN_TONE_LABELS = [
+   "Flat (ā)",
+   "Rising (á)",
+   "Dipping (ǎ)",
+   "Falling (à)"
+]
+
+CANTONESE_TONE_LABELS = [
+   "High flat (1)",
+   "Mid rising (2)",
+   "Mid flat (3)",
+   "Low falling (4)",
+   "Low rising (5)",
+   "Low flat (6)"
+]
+
+tones_df = DataFrame(tones_matrix, CANTONESE_TONE_LABELS)
+tones_df."Mandarin tone" = MANDARIN_TONE_LABELS
+tones_plot = plot(
+   [bar(tones_df, x=Symbol("Mandarin tone"), y=Symbol(y), name=y)
+      for y in CANTONESE_TONE_LABELS],
+   Layout(
+      paper_bgcolor="transparent",
+      plot_bgcolor="transparent",
+      yaxis=attr(gridcolor="#e0e6f8"),
+      font=attr(color="#e0e6f8"),
+      width=800,
+      height=500
+   )
+)
