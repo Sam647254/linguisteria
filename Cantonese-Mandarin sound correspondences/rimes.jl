9c0882de33ff8af9f540273b1ca51e962d06acc6
Sam Wang <samofcorinth@gmx.ca>
2021-10-25 02:19:22 -0700

    Added conversion to syllables

--- Cantonese-Mandarin sound correspondences/rimes.jl
+++ Cantonese-Mandarin sound correspondences/rimes.jl
@@ -2,13 +2,15 @@ include("../helpers.jl")
 
 using Unicode
 
-struct MandarinSyllable
+struct PinyinSyllable
    initial::Union{String, Nothing}
    final::String
 end
 
-function get_final(syllable::MandarinSyllable)::String
-   default(syllable.glide, "") * syllable.nucleus * default(syllable.coda, "")
+struct JyutpingSyllable
+   initial::Union{String, Nothing}
+   final::String
+   final_plosive::Union{String, Nothing}
 end
 
 PINYIN_SYLLABIC_FRICATIVES = Set([
@@ -109,15 +111,17 @@ PINYIN_NONNULL_INITIAL_FINALS = Dict([
    "üan" => "yuan"
 ])
 
-function parse_pinyin(pinyin::String)
-   normalized = Unicode.normalize(pinyin) |> collect
-   syllable = filter(isletter, normalized) |> String
+function parse_pinyin(pinyin::AbstractString)
+   normalized = Unicode.normalize(pinyin, decompose=true) |> collect
+   syllable = Unicode.normalize(filter(l -> isletter(l) || l == '\u0308', normalized) |> String,
+      compose=true)
 
    if syllable in PINYIN_SYLLABIC_FRICATIVES
-      return MandarinSyllable(syllable[1:end-1], "_")
+      return PinyinSyllable(syllable[1:end-1], "_")
    end
 
-   initial = PINYIN_INITIALS[findfirst(i -> startswith(syllable, i), PINYIN_INITIALS)]
+   initial_index = findfirst(i -> startswith(syllable, i), PINYIN_INITIALS)
+   initial = initial_index === nothing ? nothing : PINYIN_INITIALS[initial_index]
    
    final = begin
       remaining = if initial === nothing
@@ -141,5 +145,133 @@ function parse_pinyin(pinyin::String)
       error("Invalid pinyin: $pinyin")
    end
 
-   MandarinSyllable(initial, final)
+   pseudo_initial = if initial === nothing &&
+      ((startswith(final, "y") && !startswith(final, "yu")) || startswith(final, "w"))
+      final[1:1]
+   end
+
+   PinyinSyllable(default(pseudo_initial, initial), final)
+end
+
+JYUTPING_SYLLABIC_NASALS = ["m", "ng"]
+
+JYUTPING_INITIALS = [
+   "ng",
+   "gw",
+   "kw",
+   "b",
+   "p",
+   "m",
+   "f",
+   "d",
+   "t",
+   "n",
+   "s",
+   "l",
+   "z",
+   "c",
+   "j",
+   "g",
+   "k",
+   "w",
+   "h"
+]
+
+JYUTPING_FINALS = [
+   "aa",
+   "aai",
+   "aau",
+   "aam",
+   "aan",
+   "aang",
+   "aap",
+   "aat",
+   "aak",
+   "a",
+   "ai",
+   "au",
+   "am",
+   "an",
+   "ang",
+   "ap",
+   "at",
+   "ak",
+   "e",
+   "ei",
+   "eu",
+   "em",
+   "eng",
+   "ep",
+   "ek",
+   "i",
+   "iu",
+   "im",
+   "in",
+   "ing",
+   "ip",
+   "it",
+   "ik",
+   "o",
+   "oi",
+   "ou",
+   "on",
+   "ong",
+   "ot",
+   "ok",
+   "u",
+   "ui",
+   "un",
+   "ung",
+   "ut",
+   "uk",
+   "eoi",
+   "eon",
+   "eot",
+   "oe",
+   "oeng",
+   "oet",
+   "oek",
+   "yu",
+   "yun",
+   "yun",
+   "yut"
+]
+
+function parse_jyutping(jyutping::String)
+   syllable = filter(isletter, jyutping |> collect) |> String
+   
+   if syllable in JYUTPING_SYLLABIC_NASALS
+      return JyutpingSyllable(nothing, syllable, nothing)
+   end
+
+   initial_index = findfirst(i -> startswith(syllable, i), JYUTPING_INITIALS)
+   initial = initial_index === nothing ? nothing : JYUTPING_INITIALS[initial_index]
+
+   (final, final_plosive) = begin
+      remaining = syllable[length(default(initial, ""))+1:end]
+      final_plosive = if any(p -> endswith(remaining, p), ["p", "t", "k"])
+         remaining[end:end]
+      else
+         nothing
+      end
+
+      if remaining in JYUTPING_FINALS
+         (remaining[1:end-length(default(final_plosive, ""))], final_plosive)
+      else
+         nothing
+      end
+   end
+
+   if final === nothing
+      error("Invalid jyutping: $jyutping")
+   end
+
+   JyutpingSyllable(initial, final, final_plosive)
+end
+
+function syllable_mapping(triples)
+   monophones = filter(t -> length(t[2]) == 1, triples)
+   syllable_triples = map(monophones) do t
+      (t[1], parse_pinyin(t[2] |> only), parse_jyutping(t[3]))
+   end
 end
