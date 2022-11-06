a341282322861c0198fd89939bc7d2c6bcaffca8
Sam Wang (samofcorinth) <samofcorinth@gmx.ca>
2022-11-05 20:51:27 -0700

    Started migration to Scala

---
+++ src/main/scala/mc/MC.scala
@@ -0,0 +1,15 @@
+package mrk.blue.linguisteria
+package mc
+
+object MC {
+  def loadCharacters(): Unit = ???
+
+  def syllableMapping(): Unit = ???
+
+  def extractTones(): Unit = ???
+
+  def createSyllableStats(): Unit = ???
+}
+
+@main def main() =
+  ???
